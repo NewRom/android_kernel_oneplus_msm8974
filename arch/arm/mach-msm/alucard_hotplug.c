fb2ef859c09d256f398728e7b04c284d6ea082d2
jgcaap <jgcaap@gmail.com>
2015-05-25 09:57:24 +0100

    missing parts

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -33,11 +33,6 @@ struct hotplug_cpuinfo {
 u64 prev_cpu_wall;
 u64 prev_cpu_idle;
 #endif
-#ifdef CONFIG_STATE_NOTIFIER
-#include <linux/state_notifier.h>
-#else
-#include <linux/fb.h>
-#endif
 
 unsigned int up_load;
 unsigned int down_load;
